40ad38e68914e4a652f8a3d5ac395c63df4349b4
Angel Nahum Gerardo Alvarez Moreno <angel.alvarezm@uanl.edu.mx>
2025-05-14 16:22:18 -0600

    Creando la estructura del codigo

--- PIA-ARCHIVOS/muebles_oficina.py
+++ PIA-ARCHIVOS/muebles_oficina.py
@@ -1,1 +1,35 @@
 #Vamos a empezar a hacer el PIA
+
+#Clases
+
+#Estructura
+
+while True:
+    print("- Menu para Muebles de Oficina -")
+    print("1.- Agregar producto")
+    print("2.- Agregar comentario")
+    print("3.- Ver producto")
+    print("4.- Ver productos")
+    print("5.- Eliminar producto")
+    print("6.- Modificar producto")
+    print("7.- Salir")
+    
+    opcion = int(input("¿Que opcion que deseas trabajar?"))
+    
+    if opcion == 1:
+        pass
+    if opcion == 2:
+        pass
+    if opcion == 3:
+        pass
+    if opcion == 4:
+        pass
+    if opcion == 5:
+        pass
+    if opcion == 6:
+        pass
+    if opcion == 7:
+        print("¡Cerrando el programa!")
+        break
+    else:
+        print("Opcion no valida, ingrese otro valor.")
